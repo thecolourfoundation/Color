ce4bc5cd608c0b19230ce021777609970185ff40
thecolourfoundation <buildwithcolours@gmail.com>
2026-06-09 07:11:42 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,16 +2,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/tests/**/*.test.ts"],
-  collectCoverageFrom: ["src/**/*.ts"],
-
-  coverageThreshold: {
-    global: {
-      branches:   50,
-      functions:  60,
-      lines:      60,
-      statements: 60,
-    },
-  },
 
   transform: {
     "^.+\\.ts$": [
