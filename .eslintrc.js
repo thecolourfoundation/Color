ffbc6cb64489288eee1acf1f4b709c7e62282496
thecolourfoundation <buildwithcolours@gmail.com>
2026-06-09 06:54:23 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,12 @@ module.exports = {
   },
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "warn"
-  }
+    "@typescript-eslint/no-unused-vars": "warn",
+    "no-empty": "warn"
+  },
+  ignorePatterns: [
+    "src/demos/",
+    "src/benchmark/",
+    "dist/"
+  ]
 };
